f9a7ef3d679301d61e7408caa8d16a04bbf467b0
Silver4sh <thesirait01@gmail.com>
2022-12-16 21:30:51 +0700

    Create mini_game.py

---
+++ mini-progam/mini_games/mini_game.py
@@ -0,0 +1,27 @@
+import os
+import tkinter as tk
+
+## Choose game
+def game(choose_game):
+    if choose_game == 1:
+        import guess_number
+    elif choose_game == 2:
+       import rock_paper_scissors
+    elif choose_game == 3:
+        import rolling_dice
+    else :
+        return ("i'm sorry %s not exist", choose_game)
+
+## Main Menu
+class mini_games:
+    list_game = 'Guess Number [1], Rock Paper Scissor [2], Rolling Dice [3]'
+    def __init__(self):
+        self.name = str(input("What's your name ?\n"))
+        print('Hello, ', self.name)
+        print(mini_games.list_game)
+        self.game = int(input('What game you wanna play ?\n'))
+        game(self.game)      
+        
+    
+## Running
+menu = mini_games()
