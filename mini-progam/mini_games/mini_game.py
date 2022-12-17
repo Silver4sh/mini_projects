2c5388ef2123d23c2b086978018bc5e6899c3bf7
Silver4sh <thesirait01@gmail.com>
2022-12-17 17:49:27 +0700

    change name file

--- mini-progam/mini_games/mini_game.py
+++ mini-progam/mini_games/mini_game.py
@@ -6,15 +6,17 @@ def game(choose_game):
     if choose_game == 1:
         import guess_number
     elif choose_game == 2:
-       import rock_paper_scissor
+       from rock_paper_scissor import rock_paper_scissor (version 2)
     elif choose_game == 3:
         import rolling_dice
+    elif choose_game == 4:
+        from hang_man import hang_man
     else :
         return ("i'm sorry %s not exist", choose_game)
 
 ## Main Menu
 class mini_games:
-    list_game = 'Guess Number [1], Rock Paper Scissor [2], Rolling Dice [3]'
+    list_game = 'Guess Number [1], Rock Paper Scissor [2], Rolling Dice [3], Hang Man[4]'
     def __init__(self):
         self.name = str(input("What's your name ?\n"))
         print('Hello, ', self.name)
